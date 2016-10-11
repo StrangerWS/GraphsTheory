c27e168721011a99f0bce1a4810240880dbcdbcf
StrangerWS <strangernf@gmail.com>
2016-10-11 16:59:53 +0300

    Rejected commit: Default

--- src/com/strangerws/graphstheory/newgraph/NewNode.java
+++ src/com/strangerws/graphstheory/newgraph/NewNode.java
@@ -7,16 +7,17 @@ import java.util.ArrayList;
  */
 public class NewNode {
     String name;
-    ArrayList<String> ins;
-    ArrayList<String> outs;
+    ArrayList<NewNode> ins;
+    ArrayList<NewNode> outs;
 
     public NewNode(String name) {
         this.name = name;
     }
 
-    public NewNode(ArrayList<String> ins, ArrayList<String> outs, String name) {
+    public NewNode(ArrayList<NewNode> ins, ArrayList<NewNode> outs, String name) {
         this.ins = ins;
         this.outs = outs;
         this.name = name;
     }
 }
+//class Edge
