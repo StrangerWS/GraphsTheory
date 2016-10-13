86d603359b2867dc3c6ba47e12110046a59f5574
BuildTools <unconfigured@null.spigotmc.org>
2016-10-13 21:36:28 +0300

    NewEdge created

---
+++ src/com/strangerws/graphstheory/newgraph/NewEdge.java
@@ -0,0 +1,14 @@
+package com.strangerws.graphstheory.newgraph;
+
+/**
+ * Created by User on 13.10.2016.
+ */
+public class NewEdge {
+    NewNode start;
+    NewNode end;
+
+    public NewEdge(NewNode start, NewNode end) {
+        this.start = start;
+        this.end = end;
+    }
+}
